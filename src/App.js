869511c3e9ec569a49ad0b131e2cdedb9c6e2cf8
Prajwal goyal <75352182+prajwal799@users.noreply.github.com>
2021-12-02 18:36:41 +0530

    custom Sidebar sideBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 import Navbar from "./Component/Navbar.jsx";
+import Sidebar from "./Component/Sidebar";
 export default function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Sidebar />
     </div>
   );
 }
