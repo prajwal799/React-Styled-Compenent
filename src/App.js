da2b7e2c2de1d46a5876cce6df909dff702d3d3e
Prajwal goyal <75352182+prajwal799@users.noreply.github.com>
2021-12-04 00:06:20 +0530

    Navbar-media-query-use

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Navbar from "./Component/Navbar.jsx";
 import Sidebar from "./Component/Sidebar";
+import Navbar1 from "./Component/Navbar1";
 export default function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Navbar1 />
     </div>
   );
 }
